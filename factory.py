f6ad33132840f68d0edc11540acb45b590f39604
AnotherTwinkle <anothersparklingstar@gmail.com>
2022-04-18 22:02:52 +0600

    Restructure the whole site

---
+++ factory.py
@@ -0,0 +1,88 @@
+"""Site content management toolset thingy~"""
+import clicore # https://github.com/AnotherTwinkle/clicore
+import requests
+import os
+import time
+
+class WritingUtils(clicore.Module):
+	def convert_to_styled_html(self, md):
+		url = 'https://dillinger.io/factory/fetch_html'
+		payload = {
+			"name" : "target.md",
+			"unmd" : md,
+			"formatting" : True,
+			"preview" : False
+		}
+
+		response = requests.post(url, payload)
+		assert response.status_code == 200
+
+		return response.text
+
+	@clicore.command(name = 'md')
+	def md(self, ctx):
+		pass
+
+	@clicore.add_flag(name = 'output', default = f'lab/produce/out{int(time.time())}.html')
+	@md.command(name = 'parse')
+	def parse_md(self, ctx, md_path):
+		with open(md_path, 'r', encoding= 'utf-8') as file:
+			md = file.read()
+
+		html = self.convert_to_styled_html(md)
+		if not os.path.exists('lab/produce'): os.makedirs('lab/produce');
+
+		with open(ctx.flags.output , 'w', encoding= 'utf-8') as file:
+			file.write(html)
+
+	@clicore.add_flag(name = 'filename', default = 'writing.html')
+	@clicore.add_flag(name = 'date', default = None)
+	@clicore.add_flag(name = 'title', default = 'Untitled')
+	@md.command(name='publish')
+	def publis_md(self, ctx, md_path):
+		"""Automatically handle all the indexing stuff, and publish a markdown as html"""
+		with open(md_path, 'r') as file:
+			md = file.read()
+
+		filename = ctx.flags.filename
+		date = ctx.flags.date
+		title = ctx.flags.title
+		text = self.convert_to_styled_html(md)
+
+		curdir = __file__.split(os.path.sep)[:-1]
+		directory = date.split('/')[::-1] if date else 'undated'
+		path = os.path.join('writings', *directory) if date else os.path.join('writings', 'undated')
+
+		if not os.path.exists(path):
+			os.makedirs(path)
+
+		if os.path.exists(os.path.join(path, filename)):
+			raise ValueError(f"A file with the name {filename} already exists.")
+
+		with open(os.path.join(path, filename), 'w') as f:
+			f.write(text)
+
+		# We are done writing, now let's make an entry in the index table.
+		index = os.path.join('writings', 'index.html')
+		with open(index , 'r', encoding = 'utf-8') as file:
+			lines = file.read().split('\n')
+
+		start = len(lines) - 4; end = start + 1
+		fmtdate = "/".join(date.split('/')[::-1]) if date else 'undated'
+		line = f'<li class="has-line-data" data-line-start="{start}" data-line-end="{end}">{date or "Undated"}' \
+			 f'- <a href=\"/writings/{fmtdate}/{filename}\">{title}</a></li>'
+		lines.insert(-2, line)
+		html = '\n'.join(lines)
+
+		with open(index, 'w', encoding = 'utf-8') as file:
+			file.write(html)
+
+		return print('👍')
+
+def main():
+	parser = clicore.Parser()
+	parser.load_module(WritingUtils())
+	parser.run()
+
+if __name__ == '__main__':
+	main()
